d0ee3484297a9468605a655db681865add85fb8e
katharina jäger <jaeger.kt@gmail.com>
2022-02-03 17:33:38 +0100

    Create util folder and database file with products

---
+++ util/database.js
@@ -0,0 +1,45 @@
+export const productList = [
+  {
+    id: 1,
+    name: 'Strength of Character',
+    slogan: 'Ready-made lemonade',
+    note: 'Tough love from your nanna',
+    description:
+      'You need more toughness? No need to squeeze any lemons! This homemade, organic lemonade will give you everything you need to weather the unfairness of life.',
+  },
+
+  {
+    id: 2,
+    name: 'Wisdom',
+    slogan: 'Die Weißheit mit dem Löffel fressen',
+    note: 'Family-recipe handed down 6 generations',
+    description:
+      "You feel like you're always making the wrong choices? Like you can't keep up with the people around you? Try this delicious soup -your life will turn around! (spoon included)",
+  },
+
+  {
+    id: 3,
+    name: 'Coolness',
+    slogan: 'Cool as a cucumber',
+    note: 'Brand-new',
+    description:
+      'Wanna be a cool kid? We recommend this top-shelf bottled chill for all those of you lacking the calm, self-confident  demeanor of the truly cool',
+  },
+
+  {
+    id: 4,
+    name: 'Consideration',
+    slogan: 'Walk on eggshells',
+    note: 'Know someone who needs more empathy and sensitivity? Someone who needs to be more considerate of others?',
+    description: 'Need a break?',
+  },
+
+  {
+    id: 5,
+    name: 'Embarrassment',
+    slogan: 'Oh no',
+    note: 'Second-hand',
+    description:
+      "You're mad at your best friend? You think so-and-so has no shame? This might be the right gift for them.",
+  },
+];
