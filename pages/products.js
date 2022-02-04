b4c33f849ab294745273616bda3c66ff16321155
katharina jäger <jaeger.kt@gmail.com>
2022-02-04 15:59:55 +0100

    Add props and use to display all products

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,12 @@
+import { css } from '@emotion/react';
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout from '../components/Layout';
+import { productList } from '../util/database';
+
+const productCardStyles = css`
+  width: 500px;
+`;
 
 export default function Products(props) {
   return (
@@ -11,12 +18,26 @@ export default function Products(props) {
           content="products overview - bootcamp exercise"
         />
       </Head>
+      <p>
+        {props.productList.map((product) => {
+          return (
+            <div key={`productsList-${product.id}`} css={productCardStyles}>
+              <Link href={`/products/${product.id}`}>
+                <a>
+                  <h1>{product.name}</h1>
+                  <p>{product.description}</p>
+                </a>
+              </Link>
+            </div>
+          );
+        })}
+      </p>
     </Layout>
   );
 }
 
 export function getServerSideProps() {
   return {
-    props: {},
+    props: { productList: productList },
   };
 }
