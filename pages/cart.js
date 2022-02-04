4d96dd85bede00bf28a1adfd06aeefca088d31b2
katharina jäger <jaeger.kt@gmail.com>
2022-02-04 16:01:24 +0100

    Add a cart page

---
+++ pages/cart.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
+export default function Cart() {
+  return (
+    <Layout>
+      <Head>
+        <title>Shopping Cart</title>
+      </Head>
+      <p>hello this is a shopping cart</p>
+    </Layout>
+  );
+}
