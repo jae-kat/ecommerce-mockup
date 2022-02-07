fb921315ea9b91d8a63d54b7025bc9edddf34bea
katharina jäger <jaeger.kt@gmail.com>
2022-02-07 17:48:45 +0100

    Fix footer to bottom, give color

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,13 @@
+import { css } from '@emotion/react';
+
+const footerStyles = css`
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  background-color: powderblue;
+  padding: 20px;
+`;
+
 export default function Footer() {
-  return <div>This is a footer</div>;
+  return <div css={footerStyles}>This is a footer</div>;
 }
