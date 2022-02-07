c9cb6aa4bc5b78950ca34b4269c378df22a1aade
katharina jäger <jaeger.kt@gmail.com>
2022-02-07 17:50:37 +0100

    Define some styles for <main> in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,15 @@
+import { css } from '@emotion/react';
 import Head from 'next/head';
 import Footer from './Footer';
 import Header from './Header';
+
+const mainStyles = css`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 60px 30px;
+`;
 
 export default function Layout(props) {
   return (
@@ -9,7 +18,7 @@ export default function Layout(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>{props.children}</main>
+      <main css={mainStyles}>{props.children}</main>
       <Footer />
     </>
   );
