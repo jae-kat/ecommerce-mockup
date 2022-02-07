cb94a6a6ee22e5af74c627bc8d32aec81861ef1c
katharina jäger <jaeger.kt@gmail.com>
2022-02-07 17:48:09 +0100

    Add images to the product objects

--- util/database.js
+++ util/database.js
@@ -6,6 +6,8 @@ export const productList = [
     note: 'Tough love from your nanna',
     description:
       'You need more toughness? No need to squeeze any lemons! This homemade, organic lemonade will give you everything you need to weather the unfairness of life.',
+    image:
+      'https://www.motelamiio.com/out/pictures/generated/product/1/737_921_80/whitagram-image-113_bearb.jpg',
   },
 
   {
@@ -15,6 +17,8 @@ export const productList = [
     note: 'Family-recipe handed down 6 generations',
     description:
       "You feel like you're always making the wrong choices? Like you can't keep up with the people around you? Try this delicious soup -your life will turn around! (spoon included)",
+    image:
+      'https://www.motelamiio.com/out/pictures/generated/product/1/737_921_80/whitagram-image-149_bearb.jpg',
   },
 
   {
@@ -24,6 +28,8 @@ export const productList = [
     note: 'Brand-new',
     description:
       'Wanna be a cool kid? We recommend this top-shelf bottled chill for all those of you lacking the calm, self-confident  demeanor of the truly cool',
+    image:
+      'https://www.motelamiio.com/out/pictures/generated/product/2/600_750_80/motelamio-areia-mueslischale-azure-4.jpg',
   },
 
   {
@@ -32,6 +38,8 @@ export const productList = [
     slogan: 'Walk on eggshells',
     note: 'Know someone who needs more empathy and sensitivity? Someone who needs to be more considerate of others?',
     description: 'Need a break?',
+    image:
+      'https://www.motelamiio.com/out/pictures/generated/product/2/600_750_80/motel-a-miio-joao-rose-blue-speckled-s-6446_bearb.jpg',
   },
 
   {
@@ -41,5 +49,7 @@ export const productList = [
     note: 'Second-hand',
     description:
       "You're mad at your best friend? You think so-and-so has no shame? This might be the right gift for them.",
+    image:
+      'https://www.motelamiio.com/out/pictures/generated/product/1/1000_1250_80/motelamio-areia-tasse-pink-4.jpg',
   },
 ];
