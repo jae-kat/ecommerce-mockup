a528fc39d4e2b8edc64f137b0f36e3b973e5add0
katharina jäger <jaeger.kt@gmail.com>
2022-02-08 16:28:03 +0100

    Write cookie function add to cart/change amount

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,5 +1,7 @@
 import { css } from '@emotion/react';
+import Cookies from 'js-cookie';
 import Head from 'next/head';
+import { useState } from 'react';
 import Layout from '../../components/Layout';
 import { productList } from '../../util/database';
 
@@ -8,6 +10,80 @@ const productDivStyles = css`
 `;
 
 export default function ProductId(props) {
+  const [productCookies, setProductCookies] = useState(props.cookies);
+  // is the product already in the cart?
+  const productIsInCart = productCookies.some(
+    (cookie) => cookie.id === props.product.id,
+  );
+
+  function addToCart() {
+    // get the current cookies
+    const currentCartCookies = JSON.parse(Cookies.get('cart') || '[]');
+
+    // update the cookies
+    let newCookies;
+
+    // delete if it's already in the cart
+    if (productIsInCart) {
+      newCookies = currentCartCookies.filter(
+        (cookie) => cookie.id !== props.product.id,
+      );
+    } else {
+      // add if it's not yet in the cart
+      newCookies = [...currentCartCookies, { id: props.product.id, amount: 1 }];
+    }
+
+    // update cookies and state
+    Cookies.set('cart', JSON.stringify(newCookies));
+    setProductCookies(newCookies);
+  }
+
+  function addOne() {
+    // get the current amount
+    const currentCartCookies = JSON.parse(Cookies.get('cart'));
+    // update the amount
+    const newCookies = currentCartCookies.map((cookie) => {
+      if (cookie.id === props.product.id) {
+        return { ...cookie, amount: cookie.amount + 1 };
+      } else {
+        return cookie;
+      }
+    });
+    // update cookies and state
+    Cookies.set('cart', JSON.stringify(newCookies));
+    setProductCookies(newCookies);
+  }
+
+  function removeOne() {
+    // get the current amount
+    const currentCartCookies = JSON.parse(Cookies.get('cart'));
+
+    let newCookies;
+    const productCookie = currentCartCookies.find(
+      (cookie) => cookie.id === props.product.id,
+    );
+
+    // if the amount minus one is zero, delete the item from the cookies
+    // else update the cookies
+    if (productCookie.amount - 1 === 0) {
+      newCookies = currentCartCookies.filter(
+        (cookie) => cookie.id !== props.product.id,
+      );
+    } else {
+      newCookies = currentCartCookies.map((cookie) => {
+        if (cookie === productCookie) {
+          return { ...cookie, amount: cookie.amount - 1 };
+        } else {
+          return cookie;
+        }
+      });
+    }
+
+    // update cookies and state
+    Cookies.set('cart', JSON.stringify(newCookies));
+    setProductCookies(newCookies);
+  }
+
   return (
     <Layout>
       <Head>
