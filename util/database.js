e6974a89f74af615d7b898a21d52ffe761f9c955
katharina jäger <jaeger.kt@gmail.com>
2022-02-09 17:37:51 +0100

    Delete array, use database instead

--- util/database.js
+++ util/database.js
@@ -1,55 +1,13 @@
-export const productList = [
-  {
-    id: '1',
-    name: 'Strength of Character',
-    slogan: 'Ready-made lemonade',
-    note: 'Tough love from your nanna',
-    description:
-      'You need more toughness? No need to squeeze any lemons! This homemade, organic lemonade will give you everything you need to weather the unfairness of life.',
-    image:
-      'https://www.motelamiio.com/out/pictures/generated/product/1/737_921_80/whitagram-image-113_bearb.jpg',
-  },
+import { config } from 'dotenv-safe';
+import postgres from 'postgres';
 
-  {
-    id: '2',
-    name: 'Wisdom',
-    slogan: 'Die Weißheit mit dem Löffel fressen',
-    note: 'Family-recipe handed down 6 generations',
-    description:
-      "You feel like you're always making the wrong choices? Like you can't keep up with the people around you? Try this delicious soup -your life will turn around! (spoon included)",
-    image:
-      'https://www.motelamiio.com/out/pictures/generated/product/1/737_921_80/whitagram-image-149_bearb.jpg',
-  },
+config();
 
-  {
-    id: '3',
-    name: 'Coolness',
-    slogan: 'Cool as a cucumber',
-    note: 'Brand-new',
-    description:
-      'Wanna be a cool kid? We recommend this top-shelf bottled chill for all those of you lacking the calm, self-confident  demeanor of the truly cool',
-    image:
-      'https://www.motelamiio.com/out/pictures/generated/product/2/600_750_80/motelamio-areia-mueslischale-azure-4.jpg',
-  },
+const sql = postgres();
 
-  {
-    id: '4',
-    name: 'Consideration',
-    slogan: 'Walk on eggshells',
-    note: 'Know someone who needs more empathy and sensitivity? Someone who needs to be more considerate of others?',
-    description: 'Need a break?',
-    image:
-      'https://www.motelamiio.com/out/pictures/generated/product/2/600_750_80/motel-a-miio-joao-rose-blue-speckled-s-6446_bearb.jpg',
-  },
-
-  {
-    id: '5',
-    name: 'Embarrassment',
-    slogan: 'Oh no',
-    note: 'Second-hand',
-    description:
-      "You're mad at your best friend? You think so-and-so has no shame? This might be the right gift for them.",
-    image:
-      'https://www.motelamiio.com/out/pictures/generated/product/1/1000_1250_80/motelamio-areia-tasse-pink-4.jpg',
-  },
-];
+export async function readAllProducts() {
+  const products = await sql`
+    SELECT * FROM products;
+  `;
+  return products;
+}
