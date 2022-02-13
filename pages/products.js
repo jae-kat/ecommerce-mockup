d5683ee892eadb8c2251f940d848d9f0bdb36d35
katharina jäger <jaeger.kt@gmail.com>
2022-02-13 17:23:04 +0100

    Add css, move elements around, pass global state

--- pages/products.js
+++ pages/products.js
@@ -1,26 +1,45 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../components/Layout';
 import ModifyCart from '../components/ModifyCart';
 import { readAllProducts } from '../util/database';
 
 const productsDiv = css`
   display: flex;
   flex-flow: row wrap;
-  gap: 10px;
+  gap: 30px;
   max-width: 950px;
   margin: 20px;
 `;
 
-const productCardStyles = css`
-  width: 300px;
+const productCardStyles = (productColor) => css`
+  width: 400px;
   border: 2px solid #ccc;
+  border: 8px solid ${productColor};
+  border-radius: 20px;
+  padding: 10px;
+  :hover {
+    outline: 6px solid ${productColor}A6;
+  }
+  a {
+    text-decoration: none;
+    color: #5f6266;
+  }
+  img {
+    width: 200px;
+    border-radius: 20px;
+  }
+  .add {
+    display: flex;
+  }
+  h1 {
+    text-align: center;
+  }
 `;
 
 export default function Products(props) {
   return (
-    <Layout>
+    <>
       <Head>
         <title>Products</title>
         <meta name="description" content="Products" />
@@ -28,25 +47,36 @@ export default function Products(props) {
       <div css={productsDiv}>
         {props.productList.map((product) => {
           return (
-            <div key={`productList-${product.id}`} css={productCardStyles}>
+            <div
+              key={`productList-${product.id}`}
+              css={productCardStyles(product.color)}
+            >
               <Link href={`/products/${product.id}`}>
                 <a>
-                  <img
-                    src={product.image}
-                    alt="placeholder example"
-                    width="300px"
-                  />
                   <h1>{product.title}</h1>
-                  <h2>{product.slogan}</h2>
-                  <p>Price per item: {product.price}</p>
                 </a>
               </Link>
-              <ModifyCart currentProduct={product.id} cookies={props.cookies} />
+              <div className="add">
+                <Link href={`/products/${product.id}`}>
+                  <a>
+                    {' '}
+                    <img src={product.image} alt="placeholder example" />{' '}
+                  </a>
+                </Link>
+                <ModifyCart
+                  currentProduct={product.id}
+                  cookies={props.cookies}
+                  cartNumber={props.cartNumber}
+                  setCartNumber={props.setCartNumber}
+                />
+              </div>
+              <h2>{product.slogan}</h2>
+              <p>Price per item: {product.price}</p>
             </div>
           );
         })}
       </div>
-    </Layout>
+    </>
   );
 }
 
