7b73ebf1cc847994f1edc42d5ed91da9a4ed2b35
katharina jäger <jaeger.kt@gmail.com>
2022-02-15 17:13:22 +0100

    Change global state name, add css, add test-ids

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import ModifyCart from '../components/ModifyCart';
 import { readAllProducts } from '../util/database';
@@ -13,27 +14,37 @@ const productsDiv = css`
 `;
 
 const productCardStyles = (productColor) => css`
-  width: 400px;
-  border: 2px solid #ccc;
-  border: 8px solid ${productColor};
-  border-radius: 20px;
+  flex: 1 1 300px;
+  border: 4px solid #ccc;
+  border-radius: 10px;
   padding: 10px;
-  :hover {
-    outline: 6px solid ${productColor}A6;
-  }
+  filter: grayscale(80%);
   a {
     text-decoration: none;
     color: #5f6266;
   }
-  img {
-    width: 200px;
-    border-radius: 20px;
-  }
   .add {
     display: flex;
+    gap: 3%;
+    align-items: flex-start;
+    justify-content: space-around;
+    img {
+      max-width: 150px;
+      flex: 1 1 150px;
+      border-radius: 10px;
+    }
+    .modifyStyles {
+      width: 60%;
+      flex: 2 1 150px;
+    }
   }
   h1 {
     text-align: center;
+  }
+  :hover {
+    border: 4px solid ${productColor};
+    outline: 2px solid ${productColor}A6;
+    filter: grayscale(0);
   }
 `;
 
@@ -44,34 +55,37 @@ export default function Products(props) {
         <title>Products</title>
         <meta name="description" content="Products" />
       </Head>
+      <h1>Our Products</h1>
       <div css={productsDiv}>
         {props.productList.map((product) => {
           return (
             <div
               key={`productList-${product.id}`}
               css={productCardStyles(product.color)}
+              data-test-id={`product-${product.id}`}
             >
               <Link href={`/products/${product.id}`}>
                 <a>
+                  {' '}
                   <h1>{product.title}</h1>
+                  <div className="add">
+                    <Image
+                      src={product.image}
+                      alt="placeholder example"
+                      width="150px"
+                      height="225px"
+                    />{' '}
+                  </div>
+                  <p>Price per door: {product.price}</p>
                 </a>
               </Link>
-              <div className="add">
-                <Link href={`/products/${product.id}`}>
-                  <a>
-                    {' '}
-                    <img src={product.image} alt="placeholder example" />{' '}
-                  </a>
-                </Link>
-                <ModifyCart
-                  currentProduct={product.id}
-                  cookies={props.cookies}
-                  cartNumber={props.cartNumber}
-                  setCartNumber={props.setCartNumber}
-                />
-              </div>
-              <h2>{product.slogan}</h2>
-              <p>Price per item: {product.price}</p>
+              <ModifyCart
+                className="modifyStyles"
+                currentProduct={product.id}
+                cookies={props.cookies}
+                cartCookies={props.cartCookies}
+                setCartCookies={props.setCartCookies}
+              />
             </div>
           );
         })}
