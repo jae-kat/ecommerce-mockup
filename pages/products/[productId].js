693070d120a4acdcadea5ff56389eab43d4bec08
katharina jäger <jaeger.kt@gmail.com>
2022-02-21 17:07:33 +0100

    Update styles

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,17 +1,44 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
+import Link from 'next/link';
 import ModifyCart from '../../components/ModifyCart';
 import { readAllProducts } from '../../util/database';
 
-const productDivStyles = (productColor) => css`
-  max-width: 500px;
-  border: 2px solid #ccc;
-  border: 8px solid ${productColor};
-  border-radius: 20px;
-  padding: 10px;
-
+const productDivStyles = css`
+  max-width: 950px;
+  padding: 1vw;
+  h1 {
+    text-align: center;
+  }
+  a {
+    text-align: left;
+    display: inline-block;
+    text-decoration: none;
+    color: #5f6266;
+    font-size: 1.4rem;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    padding: 0 0.3vw;
+    :hover {
+      background-color: #48695a66;
+    }
+  }
   img {
-    border-radius: 20px;
+    border-radius: 6px;
+    width: 40%;
+    object-fit: cover;
+  }
+  .positionItems {
+    margin-top: 5vw;
+    display: flex;
+    gap: 5vw;
+    text-align: justify;
+    @media screen and (max-width: 750px) {
+      flex-direction: column;
+      img {
+        width: 100%;
+      }
+    }
   }
 `;
 
@@ -20,26 +47,36 @@ export default function ProductId(props) {
     <>
       <Head>
         <title>{props.product.title}</title>
-        <meta name="description" content={props.product.title} />
+        <meta
+          name="description"
+          content={`Learn more about our products: ${props.product.title}`}
+        />
       </Head>
-      <div css={productDivStyles(props.product.color)}>
+      <div css={productDivStyles}>
+        <Link href="/products">
+          <a aria-label="go back to the shop">🠔</a>
+        </Link>
         <h1>{props.product.title}</h1>
-        <img
-          src={props.product.image}
-          alt="the product"
-          width="500px"
-          data-test-id="product-image"
-        />
-        <p>{props.product.description}</p>
-        <p data-test-id="product-price">
-          Price per item: {props.product.price}
-        </p>
-        <ModifyCart
-          currentProduct={props.product.id}
-          cookies={props.cookies}
-          cartCookies={props.cartCookies}
-          setCartCookies={props.setCartCookies}
-        />
+        <div className="positionItems">
+          <img
+            src={props.product.image}
+            alt={props.product.alt}
+            width="500px"
+            data-test-id="product-image"
+          />
+          <div>
+            <p>{props.product.description}</p>
+            <p>
+              € <span data-test-id="product-price">{props.product.price}</span>
+              .00
+            </p>
+            <ModifyCart
+              currentProduct={props.product.id}
+              cartCookies={props.cartCookies}
+              setCartCookies={props.setCartCookies}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
