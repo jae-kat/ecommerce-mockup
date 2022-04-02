a7e289d8e1858a640b95a02568139efe19d86c0b
katharina jäger <jaeger.kt@gmail.com>
2022-04-02 15:03:52 +0200

    Testing puppeteer config

--- integration/jest.config.mjs
+++ integration/jest.config.mjs
@@ -1,6 +1,7 @@
 const config = {
   preset: 'jest-puppeteer',
   maxWorkers: 2,
+  testTimeout: 1000000,
 };
 
 export default config;
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,6 +3,7 @@ module.exports = {
     // Comment this in if you want to see the browser
     // headless: false,
   },
+  browserContext: 'default',
   server: {
     command: 'yarn start',
   },
